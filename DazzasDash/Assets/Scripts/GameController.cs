45f565e9ce6c11a32b55c497cb5581b6d6eb8551
BlakeShortland <40289871+BlakeShortland@users.noreply.github.com>
2018-07-09 13:41:14 +0800

    Added prototyping tools and started the game controller

---
+++ DazzasDash/Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+
+	// A timer function
+	protected float Timer(float timer)
+	{
+		timer += Time.deltaTime;
+		return timer;
+	}
+}
---
+++ DazzasDash/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : GameController
+{
+
+	Vector2 spawnLocation; // Stores the level prefab storing position.
+
+	[SerializeField]
+	float spawnDelay; // The time between level spawns.
+
+	float spawnTimer = 0; // A timer.
+
+	void Start ()
+	{
+		spawnLocation = GameObject.Find("LevelPrefabSpawnLocation").transform.position; // Finds the spawn location for the level prefabs and stores it.
+	}
+	
+	void Update ()
+	{
+		spawnTimer = Timer(spawnTimer); // Runs the timer from the GameController using the spawnTimer.
+
+		if (spawnDelay != null && spawnTimer % spawnDelay == 0); // If a spawn delay is specified and the spawn timer is divisible by the spawn delay.
+		{
+
+		}
+	}
+}
