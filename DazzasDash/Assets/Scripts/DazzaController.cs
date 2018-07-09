fec30232396043301856aff51e6c32b38e778e83
Ganon <1013035@student.sae.edu.au>
2018-07-09 14:25:00 +0800

    character controller

--- DazzasDash/Assets/Scripts/DazzaController.cs
+++ DazzasDash/Assets/Scripts/DazzaController.cs
@@ -6,48 +6,63 @@ public class DazzaController : MonoBehaviour
 {
 
     [SerializeField]
-    GameController gameController;
+    GameController gameController; // reference to the game controller script
 
-    //float the controls the speed Dazza jumps
     [SerializeField]
-    float jumpForce = 15f, maximumJumpTime = 2f;
+    float jumpForce = 15f; // the speed Dazza jumps
 
-    float jumpTimer = 0f;
+    [SerializeField]
+    float maximumJumpTime = .5f; // the maximum time dazza can ascend in his jump for
 
 
-    bool isJumping = false;
+
+
+
+    float jumpTimer = 0f; // timer to control the jump
+
+    // booleans for determines jump and grounded states
+    bool isJumping = false; 
+    bool isGrounded = false;
 
     Rigidbody2D dazzaRB;
+
+
+
+
+
+
 
     private void Awake()
     {
         dazzaRB = GetComponent<Rigidbody2D>();
     }
 
-    void Start ()
-    {
-		
-	}
-	
+
+
+
+
+
+
 	void Update ()
     {
-		if (Input.GetButtonDown("Jump"))
-        {
-            isJumping = true;
-            //jumpTimer = gameController.Timer
-        }
+        DetectUserInputs();
 
-        if (Input.GetButtonUp("Jump"))
-        {
-            isJumping = false;
-        }
-	}
+        LimitHeight();
 
+        IncrementTimer();
+
+    }
+
+
+
+
+
+
+    // makes the player jump
     private void FixedUpdate()
     {
         if (isJumping)
         {
-            //dazzaRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             dazzaRB.velocity = (Vector2.up * jumpForce);
             Debug.Log("is jumping");
         }
@@ -55,8 +70,62 @@ public class DazzaController : MonoBehaviour
 
 
 
+
+
+
+    // switches the isJumping boolean when the timer has reached the amount of time allowed to jump
     private void LimitHeight()
     {
+        if(jumpTimer >= maximumJumpTime)
+        {
+            isJumping = false;
+        }
+    }
 
+
+
+    // increments the jump timer when the player is jumping
+    private void IncrementTimer()
+    {
+        if (isJumping)
+        {
+            jumpTimer = gameController.Timer(jumpTimer);
+        }
+    }
+
+
+    // switches the jump boolean if the player presses or releases the jump button
+    private void DetectUserInputs()
+    {
+        if (Input.GetButtonDown("Jump") && jumpTimer == 0f)
+        {
+            isJumping = true;
+            isGrounded = false;
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+        }
+    }
+
+
+
+
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "CollisionMask")
+        {
+            Vector2 offset = (collision.transform.position - transform.position).normalized;
+            Debug.Log(offset);
+            if (offset.y <= -.95f)
+            {
+                isGrounded = true;
+                jumpTimer = 0f;
+            }
+        }
     }
 }
