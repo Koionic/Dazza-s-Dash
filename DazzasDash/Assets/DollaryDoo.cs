06208d8cabce30dc49c3f494318656e8bb89099e
Ganon <1013035@student.sae.edu.au>
2018-08-02 00:23:15 +0800

    Created DollaryDoooooooo

---
+++ DazzasDash/Assets/DollaryDoo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollaryDoo : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GameData gameData = FindObjectOfType<GameData>().GetComponent<GameData>();
+            gameData.dollaryDoos++;
+            Destroy(gameObject);
+        }
+    }
+}
