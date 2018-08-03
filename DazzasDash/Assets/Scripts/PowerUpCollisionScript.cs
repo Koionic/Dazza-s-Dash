4c9689af4f8c9ee45c7a43cb088f49c24eb52d3e
BlakeShortland <40289871+BlakeShortland@users.noreply.github.com>
2018-08-03 16:42:46 +0800

    Created PowerUpCollisionScript

--- DazzasDash/Assets/Scripts/PowerUpCollisionScript.cs
+++ DazzasDash/Assets/Scripts/PowerUpCollisionScript.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUpCollisionScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+public class PowerUpCollisionScript : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player"))
+		{
+			Destroy(gameObject, 0.1f);
+		}
 	}
 }
