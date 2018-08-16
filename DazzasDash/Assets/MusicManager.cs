3ae5087f34271b76ca69d96b55156a09c55d9341
Laurence <101215@student.sae.edu.au>
2018-08-16 11:44:42 +0800

    added second harbass track and completed music manager functionality

--- DazzasDash/Assets/MusicManager.cs
+++ DazzasDash/Assets/MusicManager.cs
@@ -11,6 +11,8 @@ public class MusicManager : MonoBehaviour
 
     private bool channelOnePlaying = true;
 
+    private int nextSongIndex;
+
 	// Use this for initialization
 	void Start ()
     {
@@ -20,7 +22,18 @@ public class MusicManager : MonoBehaviour
 
         channels[0].clip = songs[randSongIndex];
 
-        randSongIndex = Random.Range(0, songs.Length);
+        if (songs.Length <= 2)
+        {
+            if (randSongIndex == 0) randSongIndex = 1;
+            else randSongIndex = 0;
+        }
+        else
+        {
+            randSongIndex = FindNewSong(randSongIndex);
+
+            nextSongIndex = randSongIndex;
+        }
+
 
         channels[1].clip = songs[randSongIndex];
 
@@ -44,7 +57,9 @@ public class MusicManager : MonoBehaviour
                 channels[0].Stop();
                 channels[1].Play();
 
-                int randSongIndex = Random.Range(0, songs.Length);
+                int randSongIndex = FindNewSong(nextSongIndex);
+
+                nextSongIndex = randSongIndex;
 
                 channels[0].clip = songs[randSongIndex];
             }
@@ -55,7 +70,9 @@ public class MusicManager : MonoBehaviour
                 channels[0].Play();
                 channels[1].Stop();
 
-                int randSongIndex = Random.Range(0, songs.Length);
+                int randSongIndex = FindNewSong(nextSongIndex);
+
+                nextSongIndex = randSongIndex;
 
                 channels[1].clip = songs[randSongIndex];
             }
@@ -63,6 +80,30 @@ public class MusicManager : MonoBehaviour
         else
         {
             channels[0].loop = true;
+        }
+    }
+
+
+
+    int FindNewSong(int currentSongIndex)
+    {
+        if (currentSongIndex == 0)
+        {
+            return Random.Range(1, songs.Length);
+        }
+        else if (currentSongIndex == (songs.Length - 1))
+        {
+            return Random.Range(0, (songs.Length - 1));
+        }
+        else
+        {
+            int newIndex1 = Random.Range(0, currentSongIndex);
+            int newIndex2 = Random.Range(currentSongIndex + 1, songs.Length);
+
+            int coinToss = Random.Range(0, 2);
+
+            if (coinToss == 0) return newIndex1;
+            else return newIndex2;
         }
     }
 
