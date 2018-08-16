91cd6dd22a41045c1f12a72c93a9a7bde3a8bac0
Laurence <40289918+protodroid-IB@users.noreply.github.com>
2018-08-16 20:19:42 +0800

    started upgrade manager

--- DazzasDash/Assets/UpgradesManager.cs
+++ DazzasDash/Assets/UpgradesManager.cs
@@ -2,15 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UpgradesManager : MonoBehaviour {
+public class UpgradesManager : MonoBehaviour
+{
+    private Upgrades equipped = Upgrades.None;
+
+    private Dictionary<Upgrades, int> upgradeInventory = new Dictionary<Upgrades, int>();
+
+    private GameData gameData;
+
+    [SerializeField]
+    private int doubleCost = 3000, headStartCost = 2000, reviveCost = 1500;
+
+    private void Awake()
+    {
+        gameData = GameObject.Find("DataController").GetComponent<GameData>();
+
+        upgradeInventory.Add(Upgrades.DoubleDollaryDoos, 0);
+        upgradeInventory.Add(Upgrades.HeadStart, 0);
+        upgradeInventory.Add(Upgrades.Revive, 0);
+    }
+
+    public void Buy(Upgrades inUpgrade)
+    {
+        switch(inUpgrade)
+        {
+            case Upgrades.DoubleDollaryDoos:
+                if(gameData.dollaryDoos >= doubleCost)
+                {
+                    Add(inUpgrade);
+                }
+                break;
+            case Upgrades.HeadStart:
+                if (gameData.dollaryDoos >= headStartCost)
+                {
+                    Add(inUpgrade);
+                }
+                break;
+            case Upgrades.Revive:
+                if (gameData.dollaryDoos >= reviveCost)
+                {
+                    Add(inUpgrade);
+                }
+                break;
+        }
+    }
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    void Add(Upgrades inUpgrade)
+    {
+        int currentCount;
+        upgradeInventory.TryGetValue(inUpgrade, out currentCount);
+
+        currentCount++;
+
+        upgradeInventory.Remove(inUpgrade);
+        upgradeInventory.Add(inUpgrade, currentCount);
+
+        Debug.Log("UPGRADE: " + inUpgrade.ToString() + "\tx " + currentCount);
+    }
+
+
+    private void OnDestroy()
+    {
+        
+    }
 }
