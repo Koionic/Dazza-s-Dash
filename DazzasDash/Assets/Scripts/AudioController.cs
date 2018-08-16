54a0c1a67289f522d3d6a0276af8599370aabf70
BlakeShortland <40289871+BlakeShortland@users.noreply.github.com>
2018-08-16 21:30:03 +0800

    Created sliders, connected them to audio controller.
    Next connect those values to the audio mixer

---
+++ DazzasDash/Assets/Scripts/AudioController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+	Slider musicSlider;
+	Slider sfxSlider;
+
+	private void Start()
+	{
+		if (SceneManager.GetActiveScene().name == "Settings")
+		{
+			musicSlider = GameObject.Find("Music Slider").GetComponent<Slider>();
+			sfxSlider = GameObject.Find("SFX Slider").GetComponent<Slider>();
+		}
+	}
+}
