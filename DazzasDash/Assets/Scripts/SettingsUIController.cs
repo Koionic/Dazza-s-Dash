895946cb9bba13acb6568f117688aa7bc7945fb6
BlakeShortland <40289871+BlakeShortland@users.noreply.github.com>
2018-08-17 13:56:02 +0800

    Working on sliders to controll volume

---
+++ DazzasDash/Assets/Scripts/SettingsUIController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class SettingsUIController : MonoBehaviour
+{
+	AudioController audioController;
+
+	[SerializeField] Slider musicSlider;
+	[SerializeField] Slider sfxSlider;
+
+	void Awake()
+	{
+		audioController = GameObject.Find("DataController").GetComponent<AudioController>();
+	}
+
+	public void SetMusicSlider()
+	{
+		audioController.SetMusicSlider(musicSlider.value);
+	}
+
+	public void SetSFXSlider()
+	{
+		audioController.SetSFXSlider(musicSlider.value);
+	}
+}
